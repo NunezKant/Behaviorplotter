200d3e9379ee12e9065fe93500fda27ac9fcb828
Miguel Nunez <nunezm@hhmi.org>
2022-04-08 17:27:12 -0400

    improved plot, added KDE and performance

--- Behaviorplotter/scripts/lickplotter.py
+++ Behaviorplotter/scripts/lickplotter.py
@@ -115,6 +115,41 @@ def get_trial_categories(rewarded_trial_structure, new_trial_structure):
 
     return np.array(trial_categories), trial_counts
 
+def get_trials_with_onelick(data, xlim=(45, 110)):
+    """
+    Create a dataframe with the trials with at least one lick
+
+    Parameters
+    ----------
+    lick_df : dataframe
+        Dataframe with the lick data.
+
+    """
+    lick_df, _ = get_dataframes(data)
+    effective_trial = int(lick_df["Trial"].max())
+    _, trial_counts = get_trial_categories(
+        data["TrialRewardStrct"][:effective_trial],
+        data["TrialNewTextureStrct"][:effective_trial],
+    )
+    lick_df_wo_startflag = lick_df[lick_df["start"] != 1]
+    lick_pct = []
+    lick_pct_dict = {}
+    for key, item in trial_counts.items():
+        if item>0:
+            trials_with_licks = len(
+                lick_df_wo_startflag.loc[
+                    (lick_df_wo_startflag["Category"] == key)
+                    & (lick_df_wo_startflag["Position"] > xlim[0])
+                    & (lick_df_wo_startflag["Position"] < xlim[1])
+                ]
+                .groupby("Trial")
+                .min()["Position"]
+                .values
+            )
+            lick_pct.append(np.round(trials_with_licks / trial_counts[key], 2))
+            lick_pct_dict[key] = lick_pct[-1]
+    return lick_pct_dict
+
 def lick_plot(
     data,
     first_lick=True,
@@ -140,11 +175,13 @@ def lick_plot(
     """
 
     fig = plt.figure(figsize=fig_size)
-    grid = plt.GridSpec(5, 5, hspace=0.2, wspace=0.2)
+    grid = plt.GridSpec(5, 6, hspace=0.2, wspace=0.3)
     main_ax = fig.add_subplot(grid[1:, :4])
     x_hist = fig.add_subplot(grid[:1, :4], sharex=main_ax)
+    pct_axis = fig.add_subplot(grid[:2, 4:])
 
     lick, reward_df = get_dataframes(data)
+    lick_pct_dict = get_trials_with_onelick(data, xlim=(45, 110))
     effective_trial = int(lick["Trial"].max())
     categories, trial_counts = get_trial_categories(
         data["TrialRewardStrct"][:effective_trial],
@@ -160,6 +197,11 @@ def lick_plot(
     if first_lick:
         lick = lick.groupby("Trial").min().reset_index()
 
+    for key, value in trial_counts.items():
+        lick.loc[
+                np.where(lick["Category"] == key)[0], "Weight"
+            ] = value
+
     for category in categories:
         position = lick[lick["Category"] == category]["Position"]
         trial = lick[lick["Category"] == category]["Trial"]
@@ -173,10 +215,11 @@ def lick_plot(
             s=10,
         )
 
-        x_hist.hist(
-            bins[:-1], alpha=0.5, weights=counts / trial_counts[category], bins=bins
-        )
+        #x_hist.hist(
+        #    bins[:-1], alpha=0.5, weights=counts / trial_counts[category], bins=bins
+        #)
 
+        pct_axis.bar(category, lick_pct_dict[category]*100, alpha=0.5)
     main_ax.scatter(
         reward_df["Position"],
         reward_df.index + 1,
@@ -187,10 +230,18 @@ def lick_plot(
         alpha=0.3,
     )
 
+    sns.histplot(data=lick, hue='Category', x="Position", kde=True, weights=1/lick["Weight"], bins=bins, hue_order=categories, ax=x_hist, legend=False)
+
+    pct_axis.axhline(y=80, color='k', linestyle='--', alpha = 0.5)
+    
+
     main_ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     main_ax.set_xlabel("Position (cm)")
     main_ax.set_ylabel("Trial")
     main_ax.set_xlim(0, lick["Position"].max() + 10)
+    pct_axis.set_ylabel("% trials with one lick")
+    pct_axis.set_xlabel("")
+    pct_axis.set_ylim(0,100)
     x_hist.set_xlabel("")
     sns.despine()
 
